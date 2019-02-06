9b4f1e625c17f281e603b692fedbaa1d89f5ca86
DylanArnold101 <league@WTS5.attlocal.net>
2019-02-05 16:58:30 -0800

    Secret Message Box  Ip

--- src/ifs_ints_and_loops/SecretMessageBox.java
+++ src/ifs_ints_and_loops/SecretMessageBox.java
@@ -17,7 +17,13 @@ import javax.swing.JOptionPane;
 public class SecretMessageBox {
 
 	// 0. Make a main method and put steps 1-5 inside it
+	public static void main(String[] args) {
+		int password=1234;
+	JOptionPane.showInputDialog("What is your secret");
+	JOptionPane.showMessageDialog(null,"Your secret is safe with me");
+	JOptionPane.showInputDialog("Password");
 	
+	}
 	// 1. Set a password in a String variable
 	
 	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
---
+++ src/ifs_ints_and_loops/Unbirthday.java
@@ -0,0 +1,17 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+public static void main(String[] args) {
+	String birthday= JOptionPane.showInputDialog(null,"What's Your Birthday");
+ if(birthday.equals("02/05")) {
+ JOptionPane.showMessageDialog(null,"Cool");
+
+}
+ 
+ else {JOptionPane.showMessageDialog(null, "Very Merry Unbirthday");
+ 
+
+	
+}}}
